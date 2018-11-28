0f7f2be692b138dc3c526fab795d4dbb1045a895
jonrodsanz <jonrodsanz@hotmail.com>
2018-11-27 18:42:38 -0600

    Emojis added

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -15,7 +15,8 @@ $(function () {
   $messageForm.submit((e) => {
     e.preventDefault();
     if($messageBox.val() !== ""){
-      if(flag === true && ($messageBox.val().split("")[0] === "h") || ($messageBox.val().split("")[0] === "d")){
+      let protocol = $messageBox.val().split(":")[0]
+      if(flag === true && protocol === "http" || protocol === "data" || protocol === "https"){
         sentMessage = "<img src='" + $messageBox.val() +"' />"
       }
       else {
@@ -36,10 +37,7 @@ $(function () {
     }
   })
 
-
-
   socket.on("new message", (data) => {
-    // $chat.append("&gt " + data + "<br />")
     $chat.append(
       `<div style="padding: 0" class="col-lg-1 col-1">
         <img src="http://assets.stickpng.com/thumbs/588359a32c9eb99faafea8bc.png" width="100%" />
--- src/sockets.js
+++ src/sockets.js
@@ -1,9 +1,11 @@
 module.exports = function(io){
+  var emoji = require('node-emoji')
 
   io.on('connection', socket => {
     console.log("New user connected")
     socket.on("send message", (data) => {
       // To all the clients
+      data = emoji.emojify(data)
       io.sockets.emit("new message", data)
     })
   })
